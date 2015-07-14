031734a23982de3416cb4fe772135e691c9aae14
marinadecelis <marina88informatica@gmail.com>
2015-07-14 19:28:10 +0200

    Revert "modulo7 tema1"
    This reverts commit c2f3f231607064138923073ad22d14dea8b3227b.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,11 @@
-var models = require('../models/models.js');
-
-
 exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-	res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	})
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
-
 exports.answer = function (req, res){
-	models.Quiz.findAll().success( function (quiz) {
-		if (req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}
-	})
+	if (req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
 };
